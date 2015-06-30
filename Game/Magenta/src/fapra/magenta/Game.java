2fe02088c39342ada6619cc848637bc1441acc78
Nik N. (Nik101010) <git@nik101010.de>
2015-06-30 17:39:52 +0200

    moved api client to activity

--- Game/Magenta/src/fapra/magenta/Game.java
+++ Game/Magenta/src/fapra/magenta/Game.java
@@ -7,7 +7,6 @@ import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.View;
-import fapra.magenta.apiClient.Client;
 import fapra.magenta.audio.music.IMusicManager;
 import fapra.magenta.audio.music.MusicManager;
 import fapra.magenta.audio.music.NullMusicManager;
@@ -30,47 +29,44 @@ public class Game implements GameInterface {
 	private InputHandler inputHandler;
 	private SaveGame saveGame;
 	private TargetGenerator targetGenerator;
-	private Client apiClient;
-	private Thread apiClientThread;
 
 	// Time variables
 	private long lastTime;
 	private long thisTime;
 	private long deltaTime;
 
-    @Override
-	public void setup(Activity activity, View view, SurfaceHolder surfaceHolder, SharedPreferences preferences) {
-	    saveGame = new SaveGame();
-	    saveGame.load(activity);
+	@Override
+	public void setup(Activity activity, View view,
+			SurfaceHolder surfaceHolder, SharedPreferences preferences) {
+		saveGame = new SaveGame();
+		saveGame.load(activity);
 		inputHandler = new InputHandler();
 		renderer = new Renderer();
 		targetGenerator = new TargetGenerator(activity);
 		simulation = new Simulation(targetGenerator);
-		apiClient = new Client(activity);
-		
-		apiClientThread = new Thread(apiClient);
-		apiClientThread.start();
-		
-		if (preferences.getBoolean(activity.getString(R.string.preference_sound_key), true)) {
-	       soundManager = new SoundManager(activity);
+
+		if (preferences.getBoolean(
+				activity.getString(R.string.preference_sound_key), true)) {
+			soundManager = new SoundManager(activity);
 		} else {
-		    soundManager = new NullSoundManager(activity);
+			soundManager = new NullSoundManager(activity);
 		}
-        if (preferences.getBoolean(activity.getString(R.string.preference_music_key), true)) {
-            musicManager = new MusicManager(activity);
-        } else {
-            musicManager = new NullMusicManager(activity);
-        }
-        
+		if (preferences.getBoolean(
+				activity.getString(R.string.preference_music_key), true)) {
+			musicManager = new MusicManager(activity);
+		} else {
+			musicManager = new NullMusicManager(activity);
+		}
+
 		simulation.setup(saveGame, soundManager);
-		
+
 		view.setOnTouchListener(inputHandler);
 		print(activity);
 		lastTime = System.currentTimeMillis();
 	}
 
 	private boolean isDone = false;
-	
+
 	@Override
 	public void mainLoopIteration(Activity activity, SurfaceHolder surfaceHolder) {
 		// Calculate deltatime
@@ -79,40 +75,40 @@ public class Game implements GameInterface {
 		lastTime = thisTime;
 
 		if (!simulation.isGameOver) {
-		      // Process Input
-	        simulation.processInput(inputHandler);
-	        // Simulate World
-	        simulation.update(deltaTime);
-	        // Render World
-	        renderer.draw(surfaceHolder, simulation, deltaTime);
+			// Process Input
+			simulation.processInput(inputHandler);
+			// Simulate World
+			simulation.update(deltaTime);
+			// Render World
+			renderer.draw(surfaceHolder, simulation, deltaTime);
 		} else {
-		    //TODO Switch to gameOverScreen
-		    this.isDone = true;
-		    this.dispose();
+			// TODO Switch to gameOverScreen
+			this.isDone = true;
+			this.dispose();
 		}
 	}
-	
+
 	public void print(Activity activity) {
-	    Point out = new Point(1080, 1920);
-	    //activity.getWindowManager().getDefaultDisplay().getRealSize(out);
-	    DisplayMetrics outMetrics = new DisplayMetrics();
-	    activity.getWindowManager().getDefaultDisplay().getMetrics(outMetrics);
-	    Log.e("x", ""+ ((out.x / outMetrics.xdpi)) * 2.54f);
-	    Log.e("y", ""+ ((out.y / outMetrics.ydpi)) * 2.54f);
-	    Log.e("d", outMetrics.toString());
-	    Log.e("", out.toString());
+		Point out = new Point(1080, 1920);
+		// activity.getWindowManager().getDefaultDisplay().getRealSize(out);
+		DisplayMetrics outMetrics = new DisplayMetrics();
+		activity.getWindowManager().getDefaultDisplay().getMetrics(outMetrics);
+		Log.e("x", "" + ((out.x / outMetrics.xdpi)) * 2.54f);
+		Log.e("y", "" + ((out.y / outMetrics.ydpi)) * 2.54f);
+		Log.e("d", outMetrics.toString());
+		Log.e("", out.toString());
 	}
 
-    @Override
-    public void dispose() {
-        soundManager.dispose();
-        musicManager.dispose();
-        renderer.dispose();
-        apiClientThread.interrupt();
-    }
+	@Override
+	public void dispose() {
+		soundManager.dispose();
+		musicManager.dispose();
+		renderer.dispose();
 
-    @Override
-    public boolean isDone() {
-        return isDone;
-    }
+	}
+
+	@Override
+	public boolean isDone() {
+		return isDone;
+	}
 }
--- Game/Magenta/src/fapra/magenta/GameActivity.java
+++ Game/Magenta/src/fapra/magenta/GameActivity.java
@@ -1,6 +1,5 @@
 package fapra.magenta;
 
-import fapra.magenta.menu.MenuFragment;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
@@ -9,51 +8,74 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import fapra.magenta.apiClient.Client;
+import fapra.magenta.menu.MenuFragment;
 
 public class GameActivity extends FragmentActivity {
-    
-    Fragment currentFragment;
-    
+
+	Fragment currentFragment;
+	private Client apiClient;
+	private Thread apiClientThread;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-        PreferenceManager.setDefaultValues(this, R.xml.main_preferences, false);
-        requestWindowFeature(Window.FEATURE_NO_TITLE); 
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		PreferenceManager.setDefaultValues(this, R.xml.main_preferences, false);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_main_screen);
 
 		// Check that the activity is using the layout version with
-        // the fragment_container FrameLayout
+		// the fragment_container FrameLayout
 		View gameView = findViewById(R.id.game_fragment);
-        if (gameView != null) {
+		if (gameView != null) {
 
-            // However, if we're being restored from a previous state,
-            // then we don't need to do anything and should return or else
-            // we could end up with overlapping fragments.
-            if (savedInstanceState != null) {
-                return;
-            }
+			// However, if we're being restored from a previous state,
+			// then we don't need to do anything and should return or else
+			// we could end up with overlapping fragments.
+			if (savedInstanceState != null) {
+				return;
+			}
 
-            // Create a new Fragment to be placed in the activity layout
-            Fragment firstFragment = new MenuFragment(this);
-            currentFragment = firstFragment;
+			// Create a new Fragment to be placed in the activity layout
+			Fragment firstFragment = new MenuFragment(this);
+			currentFragment = firstFragment;
 
-            // In case this activity was started with special instructions from an
-            // Intent, pass the Intent's extras to the fragment as arguments
-            firstFragment.setArguments(getIntent().getExtras());
+			// In case this activity was started with special instructions from
+			// an
+			// Intent, pass the Intent's extras to the fragment as arguments
+			firstFragment.setArguments(getIntent().getExtras());
 
-            // Add the fragment to the 'fragment_container' FrameLayout
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.game_fragment, firstFragment).commit();
-        }
+			// Add the fragment to the 'fragment_container' FrameLayout
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.game_fragment, firstFragment).commit();
+		}
 	}
-	
+
 	public void replaceMainFragment(Fragment fragment) {
-	    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-	    transaction.replace(R.id.game_fragment, fragment);
-	    transaction.addToBackStack(fragment.toString());
-	    transaction.commit();
-	    
-	    currentFragment = fragment;
+		FragmentTransaction transaction = getSupportFragmentManager()
+				.beginTransaction().setTransition(
+						FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+		transaction.replace(R.id.game_fragment, fragment);
+		transaction.addToBackStack(fragment.toString());
+		transaction.commit();
+
+		currentFragment = fragment;
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		apiClient = new Client(this);
+
+		apiClientThread = new Thread(apiClient);
+		apiClientThread.start();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		apiClientThread.interrupt();
 	}
 }
