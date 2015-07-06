d30b11f4d098e55a0f31b325807f37ed487341b2
Nik N. (Nik101010) <git@nik101010.de>
2015-07-06 11:53:25 +0200

    implemented listener + db thread

---
+++ Game/Magenta/src/fapra/magenta/database/LineSaveThread.java
@@ -0,0 +1,34 @@
+package fapra.magenta.database;
+
+import java.util.LinkedList;
+
+import org.json.JSONException;
+
+import android.content.Context;
+import android.util.Log;
+import fapra.magenta.data.Line;
+import fapra.magenta.simulation.Simulation;
+
+public class LineSaveThread extends Thread {
+	private LinesRepository linesRepo;
+	private LinkedList<Line> lines;
+	
+	public LineSaveThread(Simulation simulation) {
+		super();
+		this.linesRepo = new LinesRepository((Context) simulation.activity);
+		this.lines = simulation.lines;
+	}
+	
+	@Override
+	public void run() {
+		for(Line line : this.lines) {
+			// TODO do not add line, when an obstacle or coin was visible
+			try {
+				linesRepo.insertLine(line.toJSON().toString());
+				Log.d("line saved", "saved line in db");
+			} catch (JSONException e) {
+				Log.e("parsing line", "failed");
+			}
+		}
+	}
+}
