9d718068ed8d6a138348415fe63f6a0090604ea2
Tim Sanwald <sanwald.tim@googlemail.com>
2015-07-11 14:22:56 +0200

    Implemented multiplier slider to heatmap

--- Heatmap/src/fapra/magenta/heatmap/MainFrame.java
+++ Heatmap/src/fapra/magenta/heatmap/MainFrame.java
@@ -1,5 +1,6 @@
 package fapra.magenta.heatmap;
 
+import java.util.Hashtable;
 import java.util.List;
 
 import javax.swing.JFrame;
@@ -21,7 +22,13 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
+
 import lib.HeatMap;
+
+import javax.swing.JSlider;
+import javax.swing.SwingConstants;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class MainFrame extends JFrame {
 
@@ -53,12 +60,13 @@ public class MainFrame extends JFrame {
         JButton btnRedraw = new JButton("Redraw");
         btnRedraw.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
-                float multiplier = 2.5f;
+                float multiplier = ((float) slider.getValue()) / 10.0f;
                 List<CombinedRow> filteredRows = Filter.queryRows(
                         Integer.parseInt(txtGridx.getText()),
                         Integer.parseInt(txtGridy.getText()),
                         Integer.parseInt(txtEndgridx.getText()),
                         Integer.parseInt(txtEndgridy.getText()));
+                System.out.println("Multiplier=" + multiplier);
                 Filter.normalizeRows(filteredRows);
                 List<Point> convertedRows = Filter.convertPoints(filteredRows);
                 System.out.println("Rows=" + convertedRows.size());
@@ -96,6 +104,29 @@ public class MainFrame extends JFrame {
         Configuration.add(txtDir);
         txtDir.setColumns(10);
         Configuration.add(btnRedraw);
+        
+        panel = new JPanel();
+        getContentPane().add(panel, BorderLayout.WEST);
+        panel.setLayout(new BorderLayout(0, 0));
+        
+        slider = new JSlider();
+        panel.add(slider);
+        slider.setMinorTickSpacing(10);
+        slider.setMaximum(50);
+        slider.setValue(10);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+        slider.setOrientation(SwingConstants.VERTICAL);
+        
+        lblValue = new JLabel("value");
+        panel.add(lblValue, BorderLayout.NORTH);
+        slider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                lblValue.setText("" + (slider.getValue() / 10.0f));
+            }
+        });
+        lblValue.setText("" + (slider.getValue() / 10.0f));
         pack();
     }
 
@@ -105,5 +136,11 @@ public class MainFrame extends JFrame {
     private JTextField txtEndgridx;
     private JTextField txtEndgridy;
     private JTextField txtDir;
+    private JSlider slider;
+    private JPanel panel;
+    private JLabel lblValue;
 
+    public JSlider getSlider() {
+        return slider;
+    }
 }
