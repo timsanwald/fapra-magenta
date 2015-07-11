6e9ca70ac5a5bf55a60f71a2c6a67d61eb3d6c72
Nik N. (Nik101010) <git@nik101010.de>
2015-07-11 12:45:49 +0200

    add device info to /api/dbLines/

--- server/app/Http/routes.php
+++ server/app/Http/routes.php
@@ -18,7 +18,29 @@ Route::get('/', function () {
 Route::controller('api/device', '\App\Http\Controllers\Api\DeviceController');
 Route::controller('api/line', '\App\Http\Controllers\Api\LineController');
 Route::get('api/dbLines/start-{startX}-{startY}/end-{endX}-{endY}', function($startX, $startY, $endX, $endY) {
-	return \App\Models\Line::with('points')->where('startGridX', $startX)->where('startGridY', $startY)->where('endGridX', $endX)->where('endGridY', $endY)->get();
+	$lines = \App\Models\Line::with('points', 'device')->where('startGridX', $startX)->where('startGridY', $startY)->where('endGridX', $endX)->where('endGridY', $endY)->get();
+
+	foreach ($lines as &$line) {
+		$line->id = (int) $line->id;
+		$line->deviceId = (int) $line->deviceId;
+		$line->startGridX = (int) $line->startGridX;
+		$line->startGridY = (int) $line->startGridY;
+		$line->endGridX = (int) $line->endGridX;
+		$line->endGridY = (int) $line->endGridY;
+		$line->startPxX = (int) $line->startPxX;
+		$line->startPxY = (int) $line->startPxY;
+		$line->endPxX = (int) $line->endPxX;
+		$line->endPxY = (int) $line->endPxY;
+		$line->startTime = (int) $line->startTime;
+		$line->endTime = (int) $line->endTime;
+		$line->isLandscape = (boolean) $line->isLandscape;
+		$line->scrollDirection = (int) $line->scrollDirection;
+		$line->complete = (boolean) $line->complete;
+	}
+
+	unset($line);
+
+	return $lines;
 });
 
 Route::get('api/dbLines', function() {
